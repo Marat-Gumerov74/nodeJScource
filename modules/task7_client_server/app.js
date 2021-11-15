d1fee8b6096c8162c964d95897e0307148e48929
Marat Gumerov <gumerovm74@gmail.com>
2021-11-15 23:22:18 +0300

    task07 server

---
+++ modules/task7_client_server/app.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  // console.log("Server request");
+  // console.log(req.url, req.method);
+  //
+  // res.setHeader('Content-Type', 'text/html' )
+  // res.write('<head><link rel="stylesheet" href="#"></head>')
+  //
+  // res.write('<h1>Hello</h1>');
+  // res.write('<p>My name is marat</p>');
+
+  res.setHeader('Content-Type', 'application/json')
+
+  const data = JSON.stringify([
+    {name: 'Tomy', age:35},
+    {name: 'Artur', age: 40}
+  ])
+  res.end(data);
+
+})
+
+server.listen(PORT, 'localhost', (error)=> {
+  error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
+
